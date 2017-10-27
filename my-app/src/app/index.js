b8a7baea275cc0da1cc9e616bcde5c6c62878d49
Kevin Lee <kevlee521@gmail.com>
2017-10-27 14:34:35 -0700

    committing react boilerplate ready to write code it

--- my-app/src/app/index.js
+++ my-app/src/app/index.js
@@ -1,2 +1,16 @@
 import React from "react";
 import { render } from "react-dom";
+
+import { Header } from "./components/Header";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+          <h1>This is a test message from Kevin</h1>
+      </div>
+    );
+  }
+}
+
+render(<App/>, window.document.getElementById("app"));
